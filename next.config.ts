169ad2b1fe0a6994f21baa8cb739b9ff8680b197
Ray7808 <yuwang7808@gmail.com>
2026-06-19 10:46:00 +0800

    fix: the wrong description of .github/workflows/deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',          // 啟用靜態導出模式
+  output: "export", // 啟用靜態導出模式
   images: {
-    unoptimized: true,       // 停用 Next.js 預設的圖片優化（GitHub Pages 必填）
+    unoptimized: true, // 停用 Next.js 預設的圖片優化（GitHub Pages 必填）
   },
   // 💡 註：因為你的 Repo 名字就是 Ray7808.github.io（個人根網域），
   // 所以這裡「不需要」設定 basePath，直接放空的或維持原樣即可。
